e7a9fd585fefa4972ecd6f611fdcfa477da30976
cosmomapanje <cosmo.mapanje@gmail.com>
2016-03-23 19:39:26 +0800

    go to Page 91

--- little.scm
+++ little.scm
@@ -234,4 +234,16 @@
 ;;; P91 "eqlist?"
 (define eqlist?
   (lambda (l1 l2)
-    (cond ((
+    (cond ((and (null? l1) (null? l2)) #t)
+	  ((and (null? l1) (atom? (car l2))) #f)
+	  ((null? l1) #f)
+	  ((and (atom? (car l1)) (null? l2)) #f)
+	  ((and (atom? (car l1)) (atom? (car l2)))
+	   (and (eqan? (car l1) (car l2))
+		(eqlist? (cdr l1) (cdr l2))))
+	  ((atom? (car l1)) #f)
+	  ((null? l2) #f)
+	  ((atom? (car l2)) #f)
+	  (else (and (eqlist? (car l1) (car l2))
+		     (eqlist? (cdr l1) (cdr l2)))))))
+
